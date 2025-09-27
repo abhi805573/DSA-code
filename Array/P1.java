dc6891dfc7b89dd63c5cc4b90cb22d52165e608a
Abhi <abhijeetkambale548@gmail.com>
2025-09-27 17:44:32 +0530

    DSA code

--- Array/P1.java
+++ Array/P1.java
@@ -4,85 +4,86 @@ public class P1 {
     public static void main(String[] args) {
 
     // 1) print the all Element in Array
-    // int arr[] = {10,150,20,45,220};
+    int arr[] = {10,150,20,45,220};
 
-    // for(int i =0; i<arr.length;i++){
-    //     System.out.println(arr[i]+"");
-    // }
-
-    // // 2)Find the MIn and max 
-    // int number[] ={5,4,8,14,50,55};
-    // int min = number[0];
-    // int max = number[0];
-
-    // for(int num : number){
-    //     if(num <min){
-    //         min = num;
-    //     }
-    //     if(num > max){
-    //         max = num;
-    //     }
-    // }
-    // System.out.println("Max num :"+ min);
-    // System.out.println("Min num :"+ max);
+    for(int i =0; i<arr.length;i++){
+        System.out.println(arr[i]+"");
+    }
 
 
-    // //3)Array Input  and print 
-    // Scanner sc =new  Scanner(System.in);
-    // System.out.println("PRint array");
+    // 2)Find the MIn and max 
+    int number[] ={5,4,8,14,50,55};
+    int min = number[0];
+    int max = number[0];
+
+    for(int num : number){
+        if(num <min){
+            min = num;
+        }
+        if(num > max){
+            max = num;
+        }
+    }
+    System.out.println("Max num :"+ min);
+    System.out.println("Min num :"+ max);
+
+
+    //3)Array Input  and print 
+    Scanner sc =new  Scanner(System.in);
+    System.out.println("PRint array");
     
-    // int JP[]= new int[5];
+    int JP[]= new int[5];
 
-    //     for(int i=0; i<JP.length;i++){
-    //         JP[i] = sc.nextInt();
-    //     }
-    //     System.out.println("-_________________-");
-    //     for(int i = 0;i<JP.length;i++){
-    //         System.out.println(JP[i]+" ");
-    //     }
+        for(int i=0; i<JP.length;i++){
+            JP[i] = sc.nextInt();
+        }
+        System.out.println("-_________________-");
+        for(int i = 0;i<JP.length;i++){
+            System.out.println(JP[i]+" ");
+        }
     
-   //  //4)Reverse the  Array 
-   //  Scanner sc = new Scanner(System.in);
-   //  System.out.println("Eneter User value");
+    //4)Reverse the  Array 
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Eneter User value");
 
-   //  int arr[] = new int [4];
-   //   for(int i =0; i<arr.length; i++){
-   //      arr[i] =sc.nextInt();
-   //   }
+    int arr[] = new int [4];
+     for(int i =0; i<arr.length; i++){
+        arr[i] =sc.nextInt();
+     }
 
-   //   System.out.println("-______________-");
+     System.out.println("-______________-");
 
-   //   for(int i = arr.length-1;i>=0;i--){
-   //      System.out.println(arr[i]+"");
-   //   }  
+     for(int i = arr.length-1;i>=0;i--){
+        System.out.println(arr[i]+"");
+     }  
 
 
-   // // 5) user  input and print element
-   //  Scanner sc = new Scanner(System.in);
-   //  System.out.println("Enter User value");
+   // 5) user  input and print element
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter User value");
 
-   //  int num = sc.nextInt();
+    int num = sc.nextInt();
 
-   //  int arr[] = new int [num];
+    int arr[] = new int [num];
 
-   //  for(int i = 0; i<arr.length;i++){
-   //    arr[i] = sc.nextInt();
-   //  }
-   //  System.out.println("-_______________-");
+    for(int i = 0; i<arr.length;i++){
+      arr[i] = sc.nextInt();
+    }
+    System.out.println("-_______________-");
 
-   //  for(int i = arr.length-1; i>=0; i--){
-   //    System.out.println(arr[i]+"");
-   //  }
+    for(int i = arr.length-1; i>=0; i--){
+      System.out.println(arr[i]+"");
+    }
 
    // 6)commulative  Example 
-//    int arr[] = {10,20,30,40,50};
+   int arr[] = {10,20,30,40,50};
 
-//       for(int i = 1; i<arr.length;i++){
-//          arr[i] = arr[i]+arr[i-1];
-//       }
-//        for(int i = 0; i<arr.length;i++){
-//            System.out.println(arr[i]+"");
-//       }
+      for(int i = 1; i<arr.length;i++){
+         arr[i] = arr[i]+arr[i-1];
+      }
+       for(int i = 0; i<arr.length;i++){
+           System.out.println(arr[i]+"");
+      }
 
      
      
